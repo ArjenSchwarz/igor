ff7502455f1d2d6732065e1036dfece48eb3341e
Arjen Schwarz <developer@arjen.eu>
2016-03-08 15:12:26 +1100

    Improvements to the javascript wrapper
    Try to get more error information on crashes and prevent the exited
    before completing request error.

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ exports.handler = function(event, context) {
     context.succeed(msg);
   })
 
+  proc.stderr.on('data', function(line){
+    var msg = new Error(line)
+    context.fail(msg);
+  })
+
   proc.on('exit', function(code){
     console.error('exit blabla: %s', code)
-    process.exit(1)
+    context.fail("No results")
   })
 }
