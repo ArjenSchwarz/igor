edb0e1154f6377459cd3b7696d9633ba1cb54364
Arjen Schwarz <developer@arjen.eu>
2016-03-23 20:25:20 +1100

    Add -server flag for running igor as server

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
+	"net/http"
+	"net/url"
 	"os"
 )
 
@@ -11,19 +14,48 @@ type message struct {
 	Value string `json:"value"`
 }
 
+var servervar bool
+
+func init() {
+	flag.BoolVar(&servervar, "server", false, "Run Igor as a server")
+	flag.Parse()
+}
+
 func main() {
-	buf := new(bytes.Buffer)
-	args := os.Args
-	event := []byte(args[1])
+	if servervar {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Path != "/" {
+				http.NotFound(w, r)
+				return
+			}
+			r.ParseForm()
+			v := url.Values{}
+			for field, values := range r.Form {
+				for _, value := range values {
+					v.Add(field, value)
+				}
+			}
+			body := body{Body: v.Encode()}
+			response := handle(body)
+			responseString, _ := json.Marshal(response)
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			w.Write(responseString)
+		})
+		http.ListenAndServe(":8080", nil)
+	} else {
+		buf := new(bytes.Buffer)
+		args := os.Args
+		event := []byte(args[1])
 
-	body := body{}
-	json.Unmarshal(event, &body)
+		body := body{}
+		json.Unmarshal(event, &body)
 
-	response := handle(body)
+		response := handle(body)
 
-	responseString, _ := json.Marshal(response)
-	fmt.Fprintf(buf, "%s", responseString)
-	buf.WriteTo(os.Stdout)
+		responseString, _ := json.Marshal(response)
+		fmt.Fprintf(buf, "%s", responseString)
+		buf.WriteTo(os.Stdout)
+	}
 }
 
 type body struct {
