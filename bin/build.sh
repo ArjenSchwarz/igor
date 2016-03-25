c0f395e0d1eb12311d5867967dc79eb6b5b05c13
Arjen Schwarz <developer@arjen.eu>
2016-03-25 18:59:54 +1100

    Improvements to build steps
    * Clean up the wercker.yml
    * Remove the debug parts from the build (for now)

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
 
-GOOS=linux GOARCH=amd64 go build -o main
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -a -ldflags '-s' -installsuffix cgo -o main
 
 zip -r igor.zip main index.js config.yml
