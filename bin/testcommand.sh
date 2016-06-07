42b9857b2bad3f1aa42ad2f0bb13697bdfaa3493
Arjen Schwarz <developer@arjen.eu>
2016-06-07 19:12:10 +1000

    Allow channel definition in testcommand

--- bin/testcommand.sh
+++ bin/testcommand.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 CONFIG_TOKEN=$(grep "^token:" config.yml | sed -e 's/token://' -e 's/ //g' -e 's/"//g')
 TEXT=$(echo $1 | sed 's/ /%20/')
-IGOR_VAR="'{\"body\":\"token=${CONFIG_TOKEN}&team_id=CCCC3333&team_domain=testdomain&channel_id=BBBB2222&channel_name=igor-testing&user_id=AAAA1111&user_name=testuser&command=%2Figor&text=${TEXT}&response_url=http://slackhook\" }'"
+CHANNEL=BBBB2222
+if [[ -n $2 ]]; then
+  CHANNEL=${2}
+fi
+IGOR_VAR="'{\"body\":\"token=${CONFIG_TOKEN}&team_id=CCCC3333&team_domain=testdomain&channel_id=${CHANNEL}&channel_name=igor-testing&user_id=AAAA1111&user_name=testuser&command=%2Figor&text=${TEXT}&response_url=http://slackhook\" }'"
 /usr/bin/env bash -c "./igor ${IGOR_VAR}"
